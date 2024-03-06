d9701d3d9b0aceee1ecbc9af4cc883b1214808f0
khoadp2 <khoadp2@sp24-cs525-0802.cs.illinois.edu>
2024-03-06 13:07:18 -0600

    add in transformers

--- kp_install.sh
+++ kp_install.sh
@@ -2,3 +2,4 @@ python3.11 -m venv kpenv
 source kpenv/bin/activate
 pip install -r requirements.txt --find-links https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 python setup.py install
+pip install transformers
