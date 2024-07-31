6aece53bb8cc807139d0cd7efce6aa32c3b52922
akulsharma1 <akulsharma02@gmail.com>
2024-07-30 22:45:46 -0700

    removed utils from util file

--- util.py
+++ util.py
@@ -47,25 +47,3 @@ def create_persistent_channel(ip_addr, port):
     ]
     channel = grpc.insecure_channel(f"{ip_addr}:{port}", options=options)
     return channel
-
-class IP:
-    ADDR = "ip_addr"
-    PORT = "port"
-
-class NodeUtil:
-    NEXT_NODE_ID = "next_node_id"
-    PREV_NODE_ID = "prev_node_id"
-
-    def next_node_endpoint(node_id):
-        """
-        Returns the endpoint for next_node in etcd.
-        """
-
-        return f"nodes/{node_id}/next_node"
-    
-    def prev_node_endpoint(node_id):
-        """
-        Returns the endpoint for prev_node in etcd.
-        """
-
-        return f"nodes/{node_id}/prev_node"
