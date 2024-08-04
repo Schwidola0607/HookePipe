4d0d1cdd2f2514531c29c2ce6d0bfbec05045865
Khoa Pham <68116922+Schwidola0607@users.noreply.github.com>
2024-08-04 16:32:11 -0700

    kpham/gRPC - gRPC for control message communication (#2)
    * initial commit for gRPC
    
    * working version of gRPC for control messages

--- coordinator.py
+++ coordinator.py
@@ -1,25 +1,99 @@
 from xmlrpc.server import SimpleXMLRPCServer
-import xmlrpc.client
+import grpc
 from topology import Topology
+import hooke_pb2_grpc
+import util
+from concurrent import futures
+import sys
+import os
+import fcntl
 
 
-class Coordinator:
+class CoordinatorServicer(hooke_pb2_grpc.CoordinatorServicer):
     def __init__(self):
         self.topology = Topology()
+        self.clients = {}  # node_id -> NodeStub
+        self.channels = {}  # node_id -> channel
 
-    def node_join(self, node_id, ip_addr, port):
+    def NodeJoin(self, request, context):
+        node_id = request.node_id
+        ip_addr = request.ip_addr
+        port = request.port
+
         print(f"Node {node_id} joined")
         self.topology.append(node_id, ip_addr, port)
-        # This should be async in the future
-        self.topology.broadcast_new_topology()
+        self.channels[node_id] = util.create_persistent_channel(ip_addr, port)
+        self.clients[node_id] = hooke_pb2_grpc.NodeStub(self.channels[node_id])
 
-    def node_leave(self, node_id):
+        # This should be async in the future
+        try:
+            self.broadcast_new_topology(node_id)
+        except grpc.RpcError as e:
+            print(f"Error while broadcasting topology: {e}")
+            context.set_details(str(e))
+            context.set_code(grpc.StatusCode.INTERNAL)
+            raise e
+
+        response = util.protobuf_from_topology(self.topology)
+        return response
+
+    def NodeLeave(self, request, context):
+        node_id = request.node_id
         print(f"Node {node_id} left")
         self.topology.remove(node_id)
+
         # This should be async in the future
-        self.topology.broadcast_new_topology()
+        self.broadcast_new_topology(node_id)
+
+    def broadcast_new_topology(self, exclude_node_id=None):
+        print("Broadcasting new topology")
+
+        topology = util.protobuf_from_topology(self.topology)
+        for node_id in self.clients:
+            if node_id != exclude_node_id:
+                print("Broadcasting to", node_id)
+                try:
+                    self.clients[node_id].MembershipChanges(topology)
+                except grpc.RpcError as e:
+                    print(f"Error while broadcasting topology to {node_id}: {e}")
+                    raise e
+                print("Broadcasted to", node_id)
+        print("Broadcasting done")
+
+    def close(self):
+        print("Closing all channels")
+        for node_id in self.clients:
+            self.channels[node_id].close()
 
 
-server = SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
-server.register_instance(Coordinator())
-server.serve_forever()
+def serve(port="8080"):
+    coordinator_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    hooke_pb2_grpc.add_CoordinatorServicer_to_server(
+        CoordinatorServicer(), coordinator_server
+    )
+    coordinator_server.add_insecure_port(f"[::]:{port}")
+    try:
+        coordinator_server.start()
+        # notify()
+        print(f"Coordinator started at port {port}")
+        coordinator_server.wait_for_termination()
+    except Exception as e:
+        print(f"Coordinator error: {e}", file=sys.stderr)
+        coordinator_server.stop(0)
+        exit(1)
+
+
+def notify():
+    pipe_path = os.getenv("COORDINATOR_PIPE_PATH", "./tmp/coordinator_pipe")
+    if not os.path.exists(pipe_path):
+        os.mkfifo(pipe_path)
+    with open(pipe_path, "w") as pipe:
+        fd = pipe.fileno()
+        flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+        payload = f"COORDINATOR"
+        os.write(fd, payload.encode())
+
+
+if __name__ == "__main__":
+    serve()
--- node.py
+++ node.py
@@ -1,75 +1,98 @@
 import uuid
-from xmlrpc.server import SimpleXMLRPCServer
-import xmlrpc.client
 import sys
-import numpy as np
+from util import get_random_transformer
+import hooke_pb2_grpc
+import hooke_pb2
+import grpc
 from topology import Topology
-import random
+import util
+from concurrent import futures
 
 
-def get_random_transformer():
-    transformations = [
-        lambda x: [i * 2 for i in x],  # Double each element
-        lambda x: [i**2 for i in x],  # Square each element
-        lambda x: [
-            i + random.randint(1, 10) for i in x
-        ],  # Add a random integer between 1 and 10 to each element
-        lambda x: [
-            i - random.randint(1, 10) for i in x
-        ],  # Subtract a random integer between 1 and 10 from each element
-        lambda x: [i / 2 for i in x],  # Divide each element by 2
-        lambda x: [-i for i in x],  # Negate each element
-        lambda x: [i**0.5 for i in x],  # Square root of each element
-    ]
-    return random.choice(transformations)
+class NodeServicer(hooke_pb2_grpc.NodeServicer):
+    number_of_active_mini_batches = 4
 
-
-class Node:
     def __init__(
         self,
-        transform,
-        coordinator_client,
         port,
-        number_of_active_mini_batches=4,
         ip_addr="localhost",
     ):
-        self.version = 0
-        self.transform = transform
-        self.coordinator_client = coordinator_client
-        url = f"http://localhost:{port}"
-        self.id = str(uuid.uuid5(uuid.NAMESPACE_URL, url))
-        self.topology = Topology()
-        self.coordinator_client.node_join(self.id, ip_addr, port)
-        self.number_of_active_mini_batches = number_of_active_mini_batches
-        self.ip_addr = ip_addr
         self.port = port
+        self.ip_addr = ip_addr
+        self.version = 0
+        self.transform = get_random_transformer()
+        self.id = str(uuid.uuid5(uuid.NAMESPACE_URL, f"http://localhost:{port}"))
 
-    def forward(self, input):
-        output = self.transform(input)
-        next_node_client = self.topology.get_next_node(self.id)
-        if next_node_client is not None:
-            next_node_client.forward(
-                output
-            )  # TODO: think about underlying communication mechanism
-        else:
-            return output
-
-    def backward(self):
-        self.version += 1
-        prev_node_client = self.topology.get_prev_node(self.id)
-        if prev_node_client is not None:
-            prev_node_client.backward()  # TODO: think about underlying communication mechanism
+        self.coordinator_channel = util.create_persistent_channel("localhost", 8080)
+        self.coordinator_stub = hooke_pb2_grpc.CoordinatorStub(self.coordinator_channel)
+        try:
+            self.coordinator_stub.NodeJoin(
+                hooke_pb2.NodeInfo(
+                    ip_addr=self.ip_addr, node_id=self.id, port=self.port
+                )
+            )
+        except grpc.RpcError as e:
+            print(f"Error while joining: {e}")
+            raise e
 
     # RPC method call by coordinator
-    # In the future this  will be called asynchronously
-    # or maybe let the RPC server handle the async part
-    def membership_change(self, new_topology):
-        print(f"New topology for node {self.id}: {new_topology}")
-        self.topology = new_topology
+    def MembershipChanges(self, request, context):
+        try:
+            topology = util.protobuf_to_topology(request)
+            print(f"New topology for node {self.id}: {topology}", flush=True)
+            self.topology = topology
+            return hooke_pb2.Void()
+        except Exception as e:
+            print(f"Error while processing new topology: {e}")
+            context.set_details(str(e))
+            context.set_code(grpc.StatusCode.INTERNAL)
+            raise e
+
+    # # TODO this will be called via NCCL / Gloo
+    # def forward(self, input):
+    #     output = self.transform(input)
+    #     self.version += 1
+    #     next_id = self.topology.get_next_node_id(self.id)
+    #     if next_id is None:
+    #         return output
+    #     return self.peers[next_id].forward(output)
+
+    # # TODO this will be called via NCCL / Gloo
+    # def backward(self):
+    #     self.version += 1
+    #     prev_id = self.topology.get_prev_node_id(self.id)
+    #     if prev_id is None:
+    #         return None
+    #     return self.peers[prev_id].backward()
+
+    def leave(self):
+        try:
+            self.coordinator_stub.NodeLeave(
+                hooke_pb2.NodeInfo(
+                    ip_addr=self.ip_addr, node_id=self.id, port=self.port
+                )
+            )
+        except grpc.RpcError as e:
+            print(f"Error while leaving: {e}")
+        self.coordinator_channel.close()
+
+    def __str__(self) -> str:
+        return f"[Node] id = {self.id}, port = {self.port}"
 
 
-coordinator_client = xmlrpc.client.ServerProxy("http://localhost:8000", allow_none=True)
-port = int(sys.argv[1])
-server = SimpleXMLRPCServer(("localhost", port), allow_none=True)
-server.register_instance(Node(get_random_transformer, coordinator_client, port))
-server.serve_forever()
+def serve(port):
+    node_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    hooke_pb2_grpc.add_NodeServicer_to_server(NodeServicer(port), node_server)
+    node_server.add_insecure_port(f"[::]:{port}")
+    try:
+        node_server.start()
+        print(f"Node server started at port {port}")
+        node_server.wait_for_termination()
+    except Exception as e:
+        print(f"Node server error: {e}", file=sys.stderr)
+        node_server.stop(0)
+        exit(1)
+
+
+if __name__ == "__main__":
+    serve(int(sys.argv[1]))
