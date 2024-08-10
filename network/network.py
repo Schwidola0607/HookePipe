5204920eda5cc20af0158878f355145f88fb4431
akulsharma1 <akulsharma02@gmail.com>
2024-08-09 18:11:30 -0700

    updated port checker functions

--- network/network.py
+++ network/network.py
@@ -1,32 +1,30 @@
 import socket
+import etcd3
 
-def is_port_available(addr: str, port: int):
+def is_port_available(etcd_client: etcd3.Etcd3Client, addr: str, port: int) -> bool:
     """
-    Check if a given address:port is available by attempting to create a socket.
+    Check if a given address:port is available by checking if there exists a lease on it in etcd.
 
     Returns True if the port is available, False otherwise.
     """
+    used = etcd_client.get(f"ports/{addr}/{port}")
+    if used[0]:
+        return False
+    
+    return True
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.bind((addr, port))  # Try to bind the socket to the IP and port
-            return True         # If successful, the port is available
-        except socket.error as e:
-            print(f"Socket error: {e}")
-            return False
-
-def find_available_port(addr: str):
+def find_available_port(etcd_client: etcd3.Etcd3Client, addr: str) -> int:
     """
     Attempts to find an open port between 1111 and 44444 for the given address.
 
-    Returns the port as a string.
+    Returns the port as an int.
 
     TODO: implement lease system with etcd for ports - https://etcd.io/docs/v3.4/learning/api/#lease-api
     """
 
     # Arbitrarily chose between 1111 and 44444 for ports
     for i in range(1111, 44444):
-        if is_port_available(addr, i):
+        if is_port_available(etcd_client, addr, i):
             return i
 
     return -1
